a8fc5c598a2d8cc8e2c59fc7e5a57038189c72f5
Diandra Melo <diandramelo.pro@gmail.com>
2025-06-12 13:23:22 +0000

    Jinja code practice on model

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,1 +1,13 @@
-SELECT * FROM {{ source('stripe', 'payment') }}
+WITH payments AS (
+    SELECT * FROM {{ source('stripe', 'payment') }}
+)
+
+SELECT
+      id
+    , orderid AS order_id
+    , paymentmethod AS payment_method
+    , status
+    , amount
+    , created AS created_at
+    , _batched_at
+FROM payments
